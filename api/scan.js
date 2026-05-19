3a553c2ec8b283e9ed77f30cd62f168438a273c9
xicohh <xicohtencatlguillermosandoval@gmail.com>
2026-05-19 00:41:30 -0600

    fix gemini model

--- api/scan.js
+++ api/scan.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     // para evitar el error de mapeo interno de la propiedad 'generationConfig'.
     const model = genAI.getGenerativeModel(
       { 
-        model: 'gemini-2.0-flash',
+        model: 'gemini-1.5-flash',
         generationConfig: { responseMimeType: 'application/json' }
       }, 
       { apiVersion: 'v1' }
